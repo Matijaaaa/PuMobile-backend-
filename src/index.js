1460979d2ee853dd75b4abc73637b210baf1ddea
MatijaFabris <75133090+Matijaaaa@users.noreply.github.com>
2024-02-25 19:14:11 +0100

    obrisan jedan file, promjena

--- src/index.js
+++ src/index.js
@@ -1,7 +1,70 @@
 import express from "express";
-import connect from "./db.js";
+import dbConnect from "./db.js";
+import "dotenv/config";
+import cors from "cors";
 
 const app = express(); // instanciranje aplikacije
 const port = 3000; // port na kojem će web server slušati
 app.get("/", (req, res) => res.send("Hello World, ovaj puta preko browsera!"));
 app.listen(port, () => console.log(`Slušam na portu ${port}!`));
+
+const corsOptions = {
+  origin: "http://localhost:8080",
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to PuMobile API");
+});
+
+// Reservation route
+app.post("/app/reservation", async (req, res) => {
+  try {
+    const db = await dbConnect();
+
+    // Use the 'reservations' collection
+    const reservationsCollection = db.collection("reservations");
+
+    // Extract reservation data from the request body
+    const {
+      selectedLocation,
+      selectedLocation2,
+      selectedTime,
+      selectedPickupTime,
+      selectedDropOffTime,
+      cardNumber,
+      expirationDate,
+      cardHolder,
+    } = req.body;
+
+    // Validate and sanitize data as needed
+
+    // Insert the reservation data into MongoDB
+    const result = await reservationsCollection.insertOne({
+      selectedLocation,
+      selectedLocation2,
+      selectedTime,
+      selectedPickupTime,
+      selectedDropOffTime,
+      cardNumber,
+      expirationDate,
+      cardHolder,
+    });
+
+    // Respond with a success message or the inserted document
+    res
+      .status(201)
+      .json({ message: "Reservation saved successfully", data: result.ops[0] });
+  } catch (error) {
+    console.error("Error saving reservation:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
